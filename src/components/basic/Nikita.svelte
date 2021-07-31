0a43395e0045403f075d48516dbda4fd7452a2b8
niqitosiq <nikitashtanko@yahoo.com>
2021-07-31 15:26:18 +0300

    #5 Удаление лишнего

--- src/components/basic/Nikita.svelte
+++
@@ -1,9 +0,0 @@
-<div class="Nikita">
-    <p>Некитааа аа а а а аа а.</p>
-</div>
-
-<style lang="scss">
-    .Nikita {
-        color: blue;
-    }
-</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,7 @@
 <script>
-  import Sonya from '../components/basic/Sonya.svelte'
-  import Nikita from '../components/basic/Nikita.svelte'
 </script>
 
-<div class="index">
-  <Sonya />
-  <Nikita />
-</div>
+<div class="index" />
 
 <style lang="scss">
 </style>
