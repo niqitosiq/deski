f931fe77c05d1289bdbbbe3eb06b55c2bf9269fd
Sonechqa <sona.simonova@list.ru>
2021-07-31 15:57:20 +0300

    #4 Шрифты

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,14 @@
 <script>
 </script>
 
-<div class="index" />
+<div class="index">
+    <p>Пирожок</p>
+</div>
 
 <style lang="scss">
+    p {
+        font-family: 'Gilroy';
+
+        font-weight: 700;
+    }
 </style>
